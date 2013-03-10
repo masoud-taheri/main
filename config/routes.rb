8d24baeb2f7ff72ccdd1fdbb749d0d1d697f5fd1
masoud-taheri <masoud.taheri.2008@gmail.com>
2013-03-10 13:13:12 -0700

    Masoud Vasate Kar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Main::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -16,7 +18,11 @@ Main::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+  
   # Sample resource route with options:
   #   resources :products do
   #     member do
